650f334bf99a74446b032777154a3127a549bf86
Cotea Ionut <coteaionutroberto@gmail.com>
2026-01-09 14:11:15 +0200

    part8 14

---
+++ part08-Part08_14.DictionaryOfManyTranslations/src/main/java/DictionaryOfManyTranslations.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DictionaryOfManyTranslations {
+  private HashMap<String, ArrayList<String>> dictionary;
+
+  public DictionaryOfManyTranslations(){
+    this.dictionary = new HashMap<>();
+  }
+
+  public void add(String word, String translation) {
+    this.dictionary.putIfAbsent(word, new ArrayList<>());
+    ArrayList<String> translations = this.dictionary.get(word);
+    translations.add(translation);
+  }
+
+  public ArrayList<String> translate(String word){
+    if(!this.dictionary.containsKey(word)){
+      return new ArrayList<>();
+    }
+    return dictionary.get(word);
+  }
+
+  public void remove(String word){
+    dictionary.remove(word);
+  }
+
+}
